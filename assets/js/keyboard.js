ef9ad683f3f4429dfc6b64bf09b74b373c38ad8c
Artem Suprun <oxymoron.dr@gmail.com>
2023-04-30 16:13:46 +0200

    feat: add keyboard structure array

---
+++ assets/js/keyboard.js
@@ -0,0 +1,648 @@
+const keyboard = [
+  [{
+    code: 'Backquote',
+    function: false,
+    lang: {
+      en: '`',
+      ru: 'ё',
+    },
+    width: 'regular',
+    sup: '~',
+  },
+  {
+    code: 'Digit1',
+    function: false,
+    lang: {
+      en: '1',
+      ru: '1',
+    },
+    width: 'regular',
+    sup: '!',
+  },
+  {
+    code: 'Digit2',
+    function: false,
+    lang: {
+      en: '2',
+      ru: '2',
+    },
+    width: 'regular',
+    sup: '@',
+  },
+  {
+    code: 'Digit3',
+    function: false,
+    lang: {
+      en: '3',
+      ru: '3',
+    },
+    width: 'regular',
+    sup: '#',
+  },
+  {
+    code: 'Digit4',
+    function: false,
+    lang: {
+      en: '4',
+      ru: '4',
+    },
+    width: 'regular',
+    sup: '$',
+  },
+  {
+    code: 'Digit5',
+    function: false,
+    lang: {
+      en: '5',
+      ru: '5',
+    },
+    width: 'regular',
+    sup: '%',
+  },
+  {
+    code: 'Digit6',
+    function: false,
+    lang: {
+      en: '6',
+      ru: '6',
+    },
+    width: 'regular',
+    sup: ':',
+  },
+  {
+    code: 'Digit7',
+    function: false,
+    lang: {
+      en: '7',
+      ru: '7',
+    },
+    width: 'regular',
+    sup: '?',
+  },
+  {
+    code: 'Digit8',
+    function: false,
+    lang: {
+      en: '8',
+      ru: '8',
+    },
+    width: 'regular',
+    sup: '*',
+  },
+  {
+    code: 'Digit9',
+    function: false,
+    lang: {
+      en: '9',
+      ru: '9',
+    },
+    width: 'regular',
+    sup: '(',
+  },
+  {
+    code: 'Digit0',
+    function: false,
+    lang: {
+      en: '0',
+      ru: '0',
+    },
+    width: 'regular',
+    sup: ')',
+  },
+  {
+    code: 'Minus',
+    function: false,
+    lang: {
+      en: '-',
+      ru: '-',
+    },
+    width: 'regular',
+    sup: '-',
+  },
+  {
+    code: 'Equal',
+    function: false,
+    lang: {
+      en: '=',
+      ru: '=',
+    },
+    width: 'regular',
+    sup: '+',
+  },
+  {
+    code: 'Backspace',
+    function: true,
+    lang: {
+      en: 'Backspace',
+      ru: 'Backspace',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  {
+    code: 'Delete',
+    function: true,
+    lang: {
+      en: 'Del',
+      ru: 'Del',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  ],
+  [{
+    code: 'Tab',
+    function: true,
+    lang: {
+      en: 'Tab',
+      ru: 'Tab',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  {
+    code: 'KeyQ',
+    function: false,
+    lang: {
+      en: 'q',
+      ru: 'й',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyW',
+    function: false,
+    lang: {
+      en: 'w',
+      ru: 'ц',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyE',
+    function: false,
+    lang: {
+      en: 'e',
+      ru: 'у',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyR',
+    function: false,
+    lang: {
+      en: 'r',
+      ru: 'к',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyT',
+    function: false,
+    lang: {
+      en: 't',
+      ru: 'е',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyY',
+    function: false,
+    lang: {
+      en: 'y',
+      ru: 'н',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyU',
+    function: false,
+    lang: {
+      en: 'u',
+      ru: 'г',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyI',
+    function: false,
+    lang: {
+      en: 'i',
+      ru: 'ш',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyO',
+    function: false,
+    lang: {
+      en: 'o',
+      ru: 'щ',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyP',
+    function: false,
+    lang: {
+      en: 'p',
+      ru: 'з',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'BracketLeft',
+    function: false,
+    lang: {
+      en: '[',
+      ru: 'х',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'BracketRight',
+    function: false,
+    lang: {
+      en: ']',
+      ru: 'ъ',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Backslash',
+    function: false,
+    lang: {
+      en: '\\',
+      ru: '\\',
+    },
+    width: 'regular',
+    sup: '/',
+  },
+  ],
+  [{
+    code: 'CapsLock',
+    function: true,
+    lang: {
+      en: 'CapsLock',
+      ru: 'CapsLock',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  {
+    code: 'KeyA',
+    function: false,
+    lang: {
+      en: 'a',
+      ru: 'ф',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyS',
+    function: false,
+    lang: {
+      en: 's',
+      ru: 'ы',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyD',
+    function: false,
+    lang: {
+      en: 'd',
+      ru: 'в',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyF',
+    function: false,
+    lang: {
+      en: 'f',
+      ru: 'а',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyG',
+    function: false,
+    lang: {
+      en: 'g',
+      ru: 'п',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyH',
+    function: false,
+    lang: {
+      en: 'h',
+      ru: 'р',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyJ',
+    function: false,
+    lang: {
+      en: 'j',
+      ru: 'о',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyK',
+    function: false,
+    lang: {
+      en: 'k',
+      ru: 'л',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyL',
+    function: false,
+    lang: {
+      en: 'l',
+      ru: 'д',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Semicolon',
+    function: false,
+    lang: {
+      en: ';',
+      ru: 'ж',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Quote',
+    function: false,
+    lang: {
+      en: "'",
+      ru: 'э',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Enter',
+    function: true,
+    lang: {
+      en: 'Enter',
+      ru: 'Enter',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  ],
+  [{
+    code: 'ShiftLeft',
+    function: true,
+    lang: {
+      en: 'Shift',
+      ru: 'Shift',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  {
+    code: 'KeyZ',
+    function: false,
+    lang: {
+      en: 'z',
+      ru: 'я',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyX',
+    function: false,
+    lang: {
+      en: 'x',
+      ru: 'ч',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyC',
+    function: false,
+    lang: {
+      en: 'c',
+      ru: 'с',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyV',
+    function: false,
+    lang: {
+      en: 'v',
+      ru: 'м',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyB',
+    function: false,
+    lang: {
+      en: 'b',
+      ru: 'и',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyN',
+    function: false,
+    lang: {
+      en: 'n',
+      ru: 'т',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'KeyM',
+    function: false,
+    lang: {
+      en: 'm',
+      ru: 'ь',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Comma',
+    function: false,
+    lang: {
+      en: ',',
+      ru: 'б',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Period',
+    function: false,
+    lang: {
+      en: '.',
+      ru: 'ю',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'Slash',
+    function: false,
+    lang: {
+      en: '/',
+      ru: '.',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'ArrowUp',
+    function: true,
+    lang: {
+      en: '↑',
+      ru: '↑',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'ShiftRight',
+    function: true,
+    lang: {
+      en: 'Shift',
+      ru: 'Shift',
+    },
+    width: 'extra-wide',
+    sup: false,
+  },
+  ],
+  [{
+    code: 'ControlLeft',
+    function: true,
+    lang: {
+      en: 'Ctrl',
+      ru: 'Ctrl',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  {
+    code: 'MetaLeft',
+    function: true,
+    lang: {
+      en: 'Win',
+      ru: 'Win',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  {
+    code: 'AltLeft',
+    function: true,
+    lang: {
+      en: 'Alt',
+      ru: 'Alt',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  {
+    code: 'Space',
+    function: false,
+    lang: {
+      en: ' ',
+      ru: ' ',
+    },
+    width: 'ultra-wide',
+    sup: false,
+  },
+  {
+    code: 'AltRight',
+    function: true,
+    lang: {
+      en: 'Alt',
+      ru: 'Alt',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  {
+    code: 'ControlRight',
+    function: true,
+    lang: {
+      en: 'Ctrl',
+      ru: 'Ctrl',
+    },
+    width: 'wide',
+    sup: false,
+  },
+  {
+    code: 'ArrowLeft',
+    function: true,
+    lang: {
+      en: '←',
+      ru: '←',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'ArrowDown',
+    function: true,
+    lang: {
+      en: '↓',
+      ru: '↓',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  {
+    code: 'ArrowRight',
+    function: true,
+    lang: {
+      en: '→',
+      ru: '→',
+    },
+    width: 'regular',
+    sup: false,
+  },
+  ],
+];
+
