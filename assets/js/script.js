2e4a2601177e1571d7eaa936033dc693d8e5944a
Artem Suprun <oxymoron.dr@gmail.com>
2023-04-30 16:47:29 +0200

    feat: add init method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/extensions */
+import { keyboardFragment, keyboardKeys } from './keyboard.js';
+
 class Keyboard {
   constructor() {
     this.caps = false;
@@ -5,6 +8,49 @@ class Keyboard {
   }
 
   init() {
+    // START CREATE ELEMENT
+    this.wrapper = document.createElement('main');
+    this.title = document.createElement('h1');
+    this.textarea = document.createElement('textarea');
+    this.keyboard = document.createElement('ul');
+    const keyboardRow = document.createElement('li');
+    this.description = document.createElement('p');
+    this.language = document.createElement('p');
+    // END CREATE ELEMENT
+
+    // START SETUP ELEMENT
+    this.wrapper.classList.add('wrapper');
+
+    this.title.classList.add('title');
+    this.title.textContent = 'RSS Виртуальная клавиатура';
+
+    this.textarea.autofocus = true;
+    this.textarea.classList.add('text');
+
+    this.keyboard.classList.add('keyboard');
+
+    keyboardRow.classList.add('keyboard__row');
+
+    this.description.classList.add('desc');
+    this.description.textContent = 'Клавиатура создана в операционной системе Windows';
+
+    this.language.classList.add('desc');
+    this.language.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
+    // END SETUP ELEMENT
+
+    // START ADD TO DOM
+    this.keyboard.append(keyboardFragment);
+    this.showLanguage(this.lang);
+
+    this.wrapper.append(this.title);
+    this.wrapper.append(this.textarea);
+    this.wrapper.append(this.keyboard);
+    this.wrapper.append(this.description);
+    this.wrapper.append(this.language);
+
+    document.body.append(this.wrapper);
+    // END ADD TO DOM
+
     this.createListeners();
   }
 
@@ -35,8 +81,7 @@ class Keyboard {
   showLanguage(lang, shift = false) {
   }
 
-  switchCaps(shiftKey) {
-  }
+  switchCaps(shiftKey) {}
 }
 
 window.addEventListener('DOMContentLoaded', () => {
