615e51191d122ffcd9f5ecb40e2f0e9a4944659b
Artem Suprun <oxymoron.dr@gmail.com>
2023-04-30 17:03:14 +0200

    feat: add eventListeners method

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,123 @@ class Keyboard {
   }
 
   createListeners() {
+    document.addEventListener('keydown', (e) => {
+      e.stopImmediatePropagation();
+
+      const key = document.getElementById(e.code);
+
+      if (!key) {
+        e.preventDefault();
+        return;
+      }
+
+      if (e.code === 'CapsLock' && !e.repeat) {
+        e.preventDefault();
+
+        this.caps = !this.caps;
+
+        const addRemove = this.caps ? 'add' : 'remove';
+
+        key.classList[addRemove]('active');
+
+        this.switchCaps(e.shiftKey);
+      } else {
+        key.classList.add('active');
+
+        if ((e.ctrlKey || e.metaKey) && e.altKey && !e.repeat) {
+          e.preventDefault();
+
+          this.lang = this.lang === 'ru' ? 'en' : 'ru';
+
+          localStorage.setItem('lang', this.lang);
+
+          this.showLanguage(this.lang, e.shiftKey);
+        } else if (!keyboardKeys[e.code].function) {
+          e.preventDefault();
+
+          this.insertText(key.textContent);
+        } else if (e.key === 'Shift' && !e.repeat) {
+          e.preventDefault();
+
+          this.switchCaps(e.shiftKey);
+        } else if (e.code === 'Tab') {
+          e.preventDefault();
+
+          this.insertText('\t');
+        } else if (e.code === 'Enter') {
+          e.preventDefault();
+
+          this.insertText('\n');
+        } else if (e.code === 'Backspace') {
+          e.preventDefault();
+
+          this.pressBackspace();
+        } else if (e.code === 'Delete') {
+          e.preventDefault();
+
+          this.pressDelete();
+        } else if (e.code === 'ArrowUp' && !e.isTrusted) {
+          this.arrowUp();
+        } else if (e.code === 'ArrowDown' && !e.isTrusted) {
+          this.arrowDown();
+        } else if (e.code === 'ArrowLeft' && !e.isTrusted) {
+          this.arrowLeft();
+        } else if (e.code === 'ArrowRight' && !e.isTrusted) {
+          this.arrowRight();
+        }
+      }
+    });
+
+    document.addEventListener('keyup', (e) => {
+      e.stopImmediatePropagation();
+
+      const key = document.getElementById(e.code);
+      if (!key) {
+        e.preventDefault();
+
+        return;
+      }
+
+      if (e.code !== 'CapsLock') {
+        key.classList.remove('active');
+
+        if (e.key === 'Shift') {
+          e.preventDefault();
+
+          this.switchCaps(e.shiftKey);
+        }
+      }
+    });
+
+    this.keyboard.addEventListener('click', (e) => {
+      this.textarea.focus();
+
+      const eventKeyDown = new KeyboardEvent('keydown', {
+        bubbles: true,
+
+        cancelable: true,
+
+        code: e.target.id,
+
+        view: window,
+      });
+
+      document.dispatchEvent(eventKeyDown);
+
+      this.textarea.focus();
+
+      const eventKeyUp = new KeyboardEvent('keyup', {
+        bubbles: true,
+
+        cancelable: true,
+
+        code: e.target.id,
+
+        view: window,
+      });
+
+      document.dispatchEvent(eventKeyUp);
+    });
   }
 
   insertText(chars) {
